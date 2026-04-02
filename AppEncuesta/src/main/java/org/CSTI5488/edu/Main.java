d7b2a57ebe678312816cf6feed6a270bf209e639
eliasdlc <eliasdlc2005@gmail.com>
2026-04-02 18:02:27 -0400

    Corregir Main.java y añadir dependencias base al build
      - Fix: main() ahora es public static void main(String[]) para que compile
      - Add: dependencias de MongoDB, JWT y gRPC al build.gradle
      - Add: plugin protobuf para generación de clases desde encuesta.proto

--- AppEncuesta/src/main/java/org/CSTI5488/edu/Main.java
+++ AppEncuesta/src/main/java/org/CSTI5488/edu/Main.java
@@ -4,7 +4,7 @@ import io.javalin.Javalin;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
+    public static void main(String[] args) {
         var app = Javalin.create(config -> {
             config.routes.get("/", ctx -> ctx.result("Hello World"));
         }).start(7000);
